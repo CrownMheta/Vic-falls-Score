6a6d3e062a2e0b13c1ffa4e2ec8f8e817d4065d3
Crown <mhetacrown@gmail.com>
2025-11-03 09:34:29 +0200

    Polished Up

--- Vic-Falls Scores/Password.swift
+++ Vic-Falls Scores/Password.swift
@@ -13,36 +13,51 @@ struct Password: View {
     @State private var message = ""
     
     var body: some View {
-      
-    
-            Text("Reset your password")
-                .font(.largeTitle)
-                .padding()
+        ZStack {
             
-            SecureField("Enter password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .frame(width: 380, height: .infinity)
+            Image("cup")
             
-            SecureField("Confirm password", text: $confirmPassword)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .frame(width: 380, height: .infinity)
+                .resizable()
+                .scaledToFit()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 607, height: 680)
+                .opacity(0.9)
             
-            Button("Set Password") {
-                if password == confirmPassword && !password.isEmpty {
+            VStack{
+                
+                Text("Reset your password")
+                    .font(.largeTitle)
+                    .padding()
+                
+                SecureField("Enter password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 380, height: .infinity)
+                
+                SecureField("Confirm password", text: $confirmPassword)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 380, height: .infinity)
+                
+                Button("Set Password") {
+                    if password == confirmPassword && !password.isEmpty {
+                        
+                        message = " Password set successfully!"
+                    } else {
+                        message = "Passwords do not match."
+                    }
                     
-                    message = " Password set successfully!"
-                } else {
-                    message = "Passwords do not match."
+                    
+                    
                 }
                 
+                .cornerRadius(10)
                 
-                
+                Text(message)
+                    .foregroundColor(password == confirmPassword ? .green : .red)
             }
-            
-            .cornerRadius(10)
-            
-            Text(message)
-                .foregroundColor(password == confirmPassword ? .green : .red)
         }
+        
     }
-
+}
+#Preview {
+    Password()
+}
